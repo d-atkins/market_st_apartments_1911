0ca41963320ecbaedd76a59c33fcfc1ce59a8685
David Atkins <atki1080@gmail.com>
2019-12-11 09:43:12 -0700

    Add case for renter_with_highest_rent

--- test/building_test.rb
+++ test/building_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/apartment'
 require './lib/building'
+require './lib/renter'
 
 class BuildingTest < Minitest::Test
 
@@ -9,6 +10,11 @@ class BuildingTest < Minitest::Test
     @building = Building.new
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 3, bedrooms: 2})
+    @renter1 = Renter.new("Spencer")
+    @renter2 = Renter.new("Jessie")
+    @renter3 = Renter.new("Max")
   end
 
   def test_it_exists
@@ -31,6 +37,24 @@ class BuildingTest < Minitest::Test
     @building.add_unit(@unit2)
 
     assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_renter_with_highest_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    @unit2.add_renter(@renter1)
+
+    assert_equal @renter1, @building.renter_with_highest_rent
+
+    @unit1.add_renter(@renter2)
+
+    assert_equal @renter2, @building.renter_with_highest_rent
+
+    @unit3.add_renter(@renter3)
+
+    assert_equal @renter2, @building.renter_with_highest_rent
+
   end
 
 end
