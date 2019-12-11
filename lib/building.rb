4b61fa573c3e6074ec66518ad82cbb7f9349a421
David Atkins <atki1080@gmail.com>
2019-12-11 09:43:33 -0700

    Add renter_with_highest_rent method

--- lib/building.rb
+++ lib/building.rb
@@ -14,4 +14,17 @@ class Building
     rents.sum.to_f / rents.length
   end
 
+  def renter_with_highest_rent
+    occupied_units = @units.find_all { |unit| unit.renter }
+    highest_rent_renter = nil
+    highest_rent = 0
+    occupied_units.each do |unit|
+      if unit.monthly_rent > highest_rent
+        highest_rent_renter = unit.renter
+        highest_rent = unit.monthly_rent
+      end
+    end
+    highest_rent_renter
+  end
+
 end
