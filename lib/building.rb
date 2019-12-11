a4886fbc943645a13378402255278703bbeaaacf
David Atkins <atki1080@gmail.com>
2019-12-11 10:09:07 -0700

    Refactor renter_with_highest_rent

--- lib/building.rb
+++ lib/building.rb
@@ -21,19 +21,17 @@ class Building
 
   def renter_with_highest_rent
     occupied_units = get_occupied_units
-    highest_rent_renter = nil
-    highest_rent = 0
-    occupied_units.each do |unit|
-      if unit.monthly_rent > highest_rent
-        highest_rent_renter = unit.renter
-        highest_rent = unit.monthly_rent
-      end
-    end
-    highest_rent_renter
+    max_rent = occupied_units.map { |unit| unit.monthly_rent }.max
+    max_rent_unit = occupied_units.find { |unit| unit.monthly_rent == max_rent }
+    max_rent_unit.renter if max_rent_unit
   end
 
   def annual_breakdown
-
+    occupied_units = get_occupied_units
+    breakdown = Hash.new
+    occupied_units.each do |unit|
+      breakdown[unit.renter.name] = unit.monthly_rent * 12
+    end
+    breakdown
   end
-
 end
